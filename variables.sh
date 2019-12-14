72e0b42c8bb6bfd0f33a0f557201928da9992b3b
Omkar <omkar@Apples-MacBook-Pro.local>
2019-12-14 16:40:28 +0530

    Database configuration

---
+++ variables.sh
@@ -0,0 +1,7 @@
+# Database Config
+
+export PROJECT_ONE_DB_NAME="project_one"
+export PROJECT_ONE_DB_USER="project_one_rw"
+export PROJECT_ONE_DB_PASSWORD="password123"
+export PROJECT_ONE_DB_HOST="localhost"
+export PROJECT_ONE_DB_PORT="5432"
