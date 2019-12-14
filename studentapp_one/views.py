e470e33dc5d2004b474001d3fa744dee220e6d20
Omkar <omkar@Apples-MacBook-Pro.local>
2019-12-14 17:30:33 +0530

    Function based view with request fumbling

--- studentapp_one/views.py
+++ studentapp_one/views.py
@@ -1,5 +1,25 @@
+# python imports
+import json
+
+# django level imports
 from django.shortcuts import render, HttpResponse
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
+@csrf_exempt 
 def func_view(request):
-    return HttpResponse("Hello")
+    """
+    A function based view
+    """
+    if request.method == "GET":
+        return HttpResponse("Hello")
+    if request.method == "POST":
+        return HttpResponse(request.body)
+
+
+class class_view(View):
+    """
+    A class based view
+    """
