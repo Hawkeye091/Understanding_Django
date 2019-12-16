2c3fb895fa191422fa68a2603e0212beb985472b
Omkar <omkar.jai@vogo.in>
2019-12-16 14:04:19 +0530

    Registered model to admin

--- studentapp_one/admin.py
+++ studentapp_one/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# App level imports
+from studentapp_one.models import Student, Teacher
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'address', 'dob', 'roll', 'marks']
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ['name', 'address', 'department', 'salary']
